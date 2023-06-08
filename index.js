86bd0e514a0271285243f0f125fb771b7375f656
Joy Purkistha <joypurokisthosh@gmail.com>
2023-06-08 20:03:35 +0600

    make verifyJwt

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.JWT_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4plofch.mongodb.net/?retryWrites=true&w=majority`;
 
